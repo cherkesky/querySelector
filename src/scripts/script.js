63d2453b75ee193c9153b61f5ff0bf7f78a7469f
Cherkesky <guy@cherkesky.com>
2019-10-09 13:07:05 -0500

    exercises 3&4

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,22 @@
 // Exercise 1
 const myBlogText = document.querySelector (".article__header");
 myBlogText.textContent = "Welcome to the Guy blog";
+console.log ("Exercise 1 ", myBlogText.textContent);
 // Exercise 2
 const getAllHeaders = document.querySelectorAll (".article__header");
 for (i=0; i < getAllHeaders.length; i++ ){
   getAllHeaders[i].classList.add("important");
+  console.log ("Exercise 2 ", getAllHeaders[i].classList);
 }
+
+// Exercise 3
+const removeDashed = document.querySelector (".dashed");
+removeDashed.classList.remove("dashed");
+console.log ("Exercise 3 ", removeDashed.classList);
+
+// Exercise 4
+const addClass = document.querySelector (".article__footer");
+addClass.classList.add("goldenrod");
+console.log ("Exercise 4 ", addClass.classList);
+
+
